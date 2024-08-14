170fd642dd54db24ff40c1c95f032c1a9cd5c1dd
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-14 11:17:34 +0100

    router exemples

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styles: [``]
 })
 export class SecondComponent {
-
+  activatedRoute = inject(ActivatedRoute);
+  constructor() { console.log(this.activatedRoute);}
 }
