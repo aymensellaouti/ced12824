6e1709b6fe0e83b91ce1e0c0354c814614a5eb8f
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-16 10:46:20 +0100

    preparation Auth

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { APP_API } from 'src/app/config/app-api.config';
 import { Credentials } from '../dto/credentials.dto';
 import { LoginResonse } from '../dto/login-response.dto';
+
+
+export interface ConnectedUser {
+  id: number;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
+  connecetdUser = new Subject<ConnectedUser>();
+  isLoggedIn$!: Observable<boolean>;
+  isLoggedOut$!: Observable<boolean>;
+
   constructor() {}
   login(credentials: Credentials): Observable<LoginResonse> {
     // Todo: Appeler l'api avec les credentials et retourner un observable
